536f0f3216e88014df6f6c4cbf0d459b1c286f70
liaosong <liao_song@outlook.com>
2021-06-28 22:26:00 +0800

    add three animals to pick from and my own branch key that works

--- app/src/main/java/co/songliao/branchdemo/MainActivity.kt
+++ app/src/main/java/co/songliao/branchdemo/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import co.songliao.branchdemo.entity.Animal
 import io.branch.indexing.BranchUniversalObject
 import io.branch.referral.Branch
 import io.branch.referral.BranchError
@@ -28,62 +29,72 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val seal = ""
-        val dog =""
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.btn_share).setOnClickListener {
-            val buo = BranchUniversalObject()
-                .setCanonicalIdentifier("cuteanimal/123")
-                .setTitle("Check out this cute animal!")
-                .setContentDescription("What a cute thing this is")
-                .setContentImageUrl("https://i.pinimg.com/564x/40/b1/87/40b187b746536ee61e4da9ad0b5b68d5.jpg")
-                .setContentIndexingMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
-                .setLocalIndexMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
-                .setContentMetadata(ContentMetadata().addCustomMetadata("harp", "seal"))
 
-            val lp = LinkProperties()
-                .setChannel("facebook")
-                .setFeature("sharing")
-                .setCampaign("content 123 launch")
-                .setStage("new user")
-                .addControlParameter("\$desktop_url", "http://example.com/home")
-                .addControlParameter(KEY_DEEP_LINK_PARAM, OtherActivity.KEY_DIRECT)
-            //.addControlParameter("custom_random", Long.toString(Calendar.getInstance().getTimeInMillis()))
-
-            val ss = ShareSheetStyle(
-                this@MainActivity,
-                "Check this cute thing!",
-                "This stuff is interesting: "
-            )
-                .setCopyUrlStyle(
-                    resources.getDrawable(android.R.drawable.ic_menu_send),
-                    "Copy",
-                    "Added to clipboard"
-                )
-                .setMoreOptionStyle(
-                    resources.getDrawable(android.R.drawable.ic_menu_search),
-                    "Show more"
-                )
-                .addPreferredSharingOption(SharingHelper.SHARE_WITH.FACEBOOK)
-                .addPreferredSharingOption(SharingHelper.SHARE_WITH.EMAIL)
-                .addPreferredSharingOption(SharingHelper.SHARE_WITH.MESSAGE)
-                .addPreferredSharingOption(SharingHelper.SHARE_WITH.HANGOUT)
-                .setAsFullWidthStyle(true)
-                .setSharingTitle("Share With")
-
-            buo.showShareSheet(this, lp, ss, object : Branch.BranchLinkShareListener {
-                override fun onShareLinkDialogLaunched() {}
-                override fun onShareLinkDialogDismissed() {}
-                override fun onLinkShareResponse(
-                    sharedLink: String?,
-                    sharedChannel: String?,
-                    error: BranchError?
-                ) {
-                }
-
-                override fun onChannelSelected(channelName: String) {}
-            })
+        findViewById<Button>(R.id.btn_share1).setOnClickListener {
+            share(0)
         }
+        findViewById<Button>(R.id.btn_share2).setOnClickListener {
+            share(1)
+        }
+        findViewById<Button>(R.id.btn_share3).setOnClickListener {
+            share(2)
+        }
+    }
+
+    private fun share(index: Int) {
+        val animal = Animal.getMockAnimals[index]
+        val buo = BranchUniversalObject()
+            .setCanonicalIdentifier("cuteanimal/${animal.name}")
+            .setTitle("Check out this cute ${animal.name}!")
+            .setContentDescription("What a cute ${animal.name} this is")
+            .setContentImageUrl(animal.url)
+            .setContentIndexingMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
+            .setLocalIndexMode(BranchUniversalObject.CONTENT_INDEX_MODE.PUBLIC)
+            .setContentMetadata(ContentMetadata().addCustomMetadata("harp", "seal"))
+
+        val lp = LinkProperties()
+            .setChannel("facebook")
+            .setFeature("sharing")
+            .setCampaign("content 123 launch")
+            .setStage("new user")
+            .addControlParameter("\$desktop_url", "http://example.com/home")
+            .addControlParameter(KEY_DEEP_LINK_PARAM, OtherActivity.KEY_DIRECT)
+        //.addControlParameter("custom_random", Long.toString(Calendar.getInstance().getTimeInMillis()))
+
+        val ss = ShareSheetStyle(
+            this@MainActivity,
+            "Check this cute thing!",
+            "This stuff is interesting: "
+        )
+            .setCopyUrlStyle(
+                resources.getDrawable(android.R.drawable.ic_menu_send),
+                "Copy",
+                "Added to clipboard"
+            )
+            .setMoreOptionStyle(
+                resources.getDrawable(android.R.drawable.ic_menu_search),
+                "Show more"
+            )
+            .addPreferredSharingOption(SharingHelper.SHARE_WITH.FACEBOOK)
+            .addPreferredSharingOption(SharingHelper.SHARE_WITH.EMAIL)
+            .addPreferredSharingOption(SharingHelper.SHARE_WITH.MESSAGE)
+            .addPreferredSharingOption(SharingHelper.SHARE_WITH.HANGOUT)
+            .setAsFullWidthStyle(true)
+            .setSharingTitle("Share With")
+
+        buo.showShareSheet(this, lp, ss, object : Branch.BranchLinkShareListener {
+            override fun onShareLinkDialogLaunched() {}
+            override fun onShareLinkDialogDismissed() {}
+            override fun onLinkShareResponse(
+                sharedLink: String?,
+                sharedChannel: String?,
+                error: BranchError?
+            ) {
+            }
+
+            override fun onChannelSelected(channelName: String) {}
+        })
     }
 
 
